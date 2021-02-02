8a68bab14c28a4042b7d54fbf8273647d49a30e6
bhprtk <bhprtk@gmail.com>
2021-02-02 13:26:05 -0800

    Queue and QueueTest formatted and complete

--- Lab4/src/Queue.java
+++ Lab4/src/Queue.java
@@ -1,178 +1,185 @@
+
 /**
  * Queue.java
- * @author Pratik Bhandari
  * @author Naqib Khan
+ * @author Pratik Bhandari
  */
 
 import java.util.NoSuchElementException;
 
-
 public class Queue<T> {
-    private class Node {
-        private T data;
-        private Node next;
-       
-        public Node(T data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-   
-    private int length;
-    private Node front;
-    private Node end;
-   
-    /************************************************************************* CONSTRUCTORS **/
-   
-    /**
-     * Default constructor for the Queue class
-     * @postcondition a new Queue object with all fields
-     * assigned default values
-     */
-    public Queue() {
-    	length = 0;
-    	front = end = null;
-    }
-   
-    /**
-     * Copy constructor for the Queue class
-     * @param original the Queue to copy
-     * @postcondition a new Queue object which is
-     * an identical, but distinct, copy of original
-     */
-    public Queue(Queue<T> original) {
-    	if(original == null) {
-    		return;
-    	}
-    	if(original.length == 0) {
-    		length = 0;
-    		front = end = null;
-    	} else {
-    		Node N = original.front;
-    		while(N != null) {
-    			enqueue(N.data);
-    			N = N.next;
-    		}
-    	}
-    }
-   
-    /********************************************************************** ACCESSORS **/
-   
-    /**
-     * Returns the value stored at the front
-     * of the Queue
-     * @return the value at the front of the queue
-     * @precondition !isEmpty()
-     * @throws NoSuchElementException when the
-     * precondition is violated
-     */
-    public T getFront() throws NoSuchElementException {
-    	if(isEmpty()) {
-    		throw new NoSuchElementException("getFront(): "
-    				+ "The list is empty, cannot get front!");
-    	}
-    	return front.data;
-    }
-   
-    /**
-     * Returns the length of the Queue
-     * @return the length from 0 to n
-     */
-    public int getLength() {
-        return length;
-    }
-   
-    /**
-     * Determines whether a Queue is empty
-     * @return whether the Queue is empty
-     */
-    public boolean isEmpty() {
-        return length == 0;
-    }
-   
-    /**
-     * Determines whether two Queues contain
-     * the same values in the same order
-     * @param o the Object to compare to this
-     * @return whether o and this are equal
-     */
-    @SuppressWarnings("unchecked")
-    @Override public boolean equals(Object o) {
-        if(o == this) {
-            return true;
-        } else if (!(o instanceof Queue)) {
-            return false;
-        } else {
-            Queue<T> Q = (Queue<T>) o;
-            if (this.length != Q.length) {
-                return false;
-            } else {
-                Node temp1 = this.front;
-                Node temp2 = Q.front;
-                while (temp1 != null) { //Lists are same length
-                    if (!(temp1.data.equals(temp2.data))) {
-                        return false;
-                    }
-                    temp1 = temp1.next;
-                    temp2 = temp2.next;
-                }
-                return true;
-            }
-        }
-    }
-   
-    /********************************************************************** MUTATORS **/   
-    /**
-     * Inserts a new value at the end of
-     * the Queue
-     * @param data the new data to insert
-     * @postcondition a new node at the end
-     * of the Queue
-     */
-    public void enqueue(T data) {
-	   if (length == 0) { //edge case
-	      front = end = new Node(data);
-	   } else { //general case
-	      end.next = new Node(data);
-	      end = end.next;
-	   }
-	   length++;
+	private class Node {
+		private T data;
+		private Node next;
+
+		public Node(T data) {
+			this.data = data;
+			this.next = null;
+		}
 	}
-   
-    /**
-     * Removes the front element in the Queue
-     * @precondition !isEmpty()
-     * @throws NoSuchElementException when
-     * the precondition is violated
-     * @postcondition the front element has
-     * been removed
-     */
-    public void dequeue() throws NoSuchElementException {
-    	if(isEmpty()) {
-    		throw new NoSuchElementException("dequeue(): "
-    				+ "The list is empty, cannot dequeue!");
-    	}
-    	front = front.next;
-    	length--;
-    }
-    
-    /********************************************************************** ADDITONAL OPERATIONS **/
-   
-   
-    /**
-     * Returns the values stored in the Queue
-     * as a String, separated by a blank space
-     * with a new line character at the end
-     * @return a String of Queue values
-     */
-    @Override public String toString() {
-        String result = "";
-        Node N = front;
-        while(N != null) {
-        	result += N.data + " ";
-        	N = N.next;
-        }
-        return result += "\n";
-    }   
-   
+
+	private int length;
+	private Node front;
+	private Node end;
+
+	/*************************************************************************
+	 * CONSTRUCTORS
+	 **/
+
+	/**
+	 * Default constructor for the Queue class
+	 * 
+	 * @postcondition a new Queue object with all fields assigned default values
+	 */
+	public Queue() {
+		length = 0;
+		front = end = null;
+	}
+
+	/**
+	 * Copy constructor for the Queue class
+	 * 
+	 * @param original the Queue to copy
+	 * @postcondition a new Queue object which is an identical, but distinct, copy
+	 *                of original
+	 */
+	public Queue(Queue<T> original) {
+		if (original == null) {
+			return;
+		}
+		if (original.length == 0) {
+			length = 0;
+			front = end = null;
+		} else {
+			Node N = original.front;
+			while (N != null) {
+				enqueue(N.data);
+				N = N.next;
+			}
+		}
+	}
+
+	/**********************************************************************
+	 * ACCESSORS
+	 **/
+
+	/**
+	 * Returns the value stored at the front of the Queue
+	 * 
+	 * @return the value at the front of the queue
+	 * @precondition !isEmpty()
+	 * @throws NoSuchElementException when the precondition is violated
+	 */
+	public T getFront() throws NoSuchElementException {
+		if (isEmpty()) {
+			throw new NoSuchElementException("getFront(): " + "The list is empty, cannot get front!");
+		}
+		return front.data;
+	}
+
+	/**
+	 * Returns the length of the Queue
+	 * 
+	 * @return the length from 0 to n
+	 */
+	public int getLength() {
+		return length;
+	}
+
+	/**
+	 * Determines whether a Queue is empty
+	 * 
+	 * @return whether the Queue is empty
+	 */
+	public boolean isEmpty() {
+		return length == 0;
+	}
+
+	/**
+	 * Determines whether two Queues contain the same values in the same order
+	 * 
+	 * @param o the Object to compare to this
+	 * @return whether o and this are equal
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		} else if (!(o instanceof Queue)) {
+			return false;
+		} else {
+			Queue<T> Q = (Queue<T>) o;
+			if (this.length != Q.length) {
+				return false;
+			} else {
+				Node temp1 = this.front;
+				Node temp2 = Q.front;
+				while (temp1 != null) { // Lists are same length
+					if (!(temp1.data.equals(temp2.data))) {
+						return false;
+					}
+					temp1 = temp1.next;
+					temp2 = temp2.next;
+				}
+				return true;
+			}
+		}
+	}
+
+	/**********************************************************************
+	 * MUTATORS
+	 **/
+	/**
+	 * Inserts a new value at the end of the Queue
+	 * 
+	 * @param data the new data to insert
+	 * @postcondition a new node at the end of the Queue
+	 */
+	public void enqueue(T data) {
+		if (length == 0) { // edge case
+			front = end = new Node(data);
+		} else { // general case
+			end.next = new Node(data);
+			end = end.next;
+		}
+		length++;
+	}
+
+	/**
+	 * Removes the front element in the Queue
+	 * 
+	 * @precondition !isEmpty()
+	 * @throws NoSuchElementException when the precondition is violated
+	 * @postcondition the front element has been removed
+	 */
+	public void dequeue() throws NoSuchElementException {
+		if (isEmpty()) {
+			throw new NoSuchElementException("dequeue(): " + "The list is empty, cannot dequeue!");
+		}
+		front = front.next;
+		length--;
+	}
+
+	/**********************************************************************
+	 * ADDITONAL OPERATIONS
+	 **/
+
+	/**
+	 * Returns the values stored in the Queue as a String, separated by a blank
+	 * space with a new line character at the end
+	 * 
+	 * @return a String of Queue values
+	 */
+	@Override
+	public String toString() {
+		String result = "";
+		Node N = front;
+		while (N != null) {
+			result += N.data + " ";
+			N = N.next;
+		}
+		return result += "\n";
+	}
+
 }
--- Lab4/src/QueueTest.java
+++ Lab4/src/QueueTest.java
@@ -1,49 +1,41 @@
+
+/**
+ * Queue.java
+ * @author Naqib Khan
+ * @author Pratik Bhandari
+ */
+
 import java.util.NoSuchElementException;
 
 public class QueueTest {
 	public static void main(String[] args) {
-		
+
 		System.out.println("********************************** Test(1) Constructor\n");
-		
+
 		Queue<Integer> Q1 = new Queue<>();
 		System.out.println("Should print an empty queue: " + Q1);
-		
+
 		Queue<Integer> anotherQ1 = new Queue<>();
 		System.out.println("Should print an empty queue: " + anotherQ1);
-		
-		
-		
-		
-		
-		
+
 		System.out.println("************************************* Test(2) toString()\n");
-		Queue<Integer> Q2 = new Queue<>();		
+		Queue<Integer> Q2 = new Queue<>();
 		System.out.println("Should print an empty queue: " + Q2.toString());
 
 		Q2.enqueue(1);
 		Q2.enqueue(2);
 		Q2.enqueue(3);
 		System.out.println("Should print 1 2 3: " + Q2.toString());
-		
-		
-		
-		
-		
-		
+
 		System.out.println("************************************* Test(3) enqueue()\n");
-		
+
 		Queue<Integer> Q3 = new Queue<>();
 		Q3.enqueue(1);
 		System.out.println("Should print 1: " + Q3);
 		Q3.enqueue(2);
 		Q3.enqueue(3);
 		System.out.println("Should print 1 2 3: " + Q3);
-		
-		
-		
-		
-		
-		
+
 		System.out.println("************************************* Test(4) getLength()\n");
 		Queue<Integer> Q4 = new Queue<>();
 		System.out.println("Should print 0: " + Q3.getLength());
@@ -53,9 +45,6 @@ public class QueueTest {
 		System.out.println("Should print 3: " + Q3.getLength());
 		System.out.println();
 
-		
-		
-		
 		System.out.println("************************************* Test(5) isEmpty()\n");
 		Queue<Integer> Q5 = new Queue<>();
 		System.out.println("Should print true: " + Q5.isEmpty());
@@ -64,17 +53,12 @@ public class QueueTest {
 		Q5.enqueue(3);
 		System.out.println("Should print false: " + Q5.isEmpty());
 		System.out.println();
-		
-		
-		
-		
-		
-		
+
 		System.out.println("************************************* Test(6) dequeue()\n");
 		Queue<Integer> Q6 = new Queue<>();
 		try {
 			Q6.dequeue();
-		} catch(NoSuchElementException e) {
+		} catch (NoSuchElementException e) {
 			System.out.println("Should print error: " + e.getMessage());
 		}
 		Q6.enqueue(1);
@@ -83,16 +67,11 @@ public class QueueTest {
 		Q6.dequeue();
 		System.out.println("Should print 2 3: " + Q6);
 
-		
-		
-		
-		
-		
 		System.out.println("************************************* Test(7) getFront()\n");
 		Queue<Integer> Q7 = new Queue<>();
 		try {
 			Q7.getFront();
-		} catch(NoSuchElementException e) {
+		} catch (NoSuchElementException e) {
 			System.out.println("Should print error: " + e.getMessage());
 		}
 		Q7.enqueue(1);
@@ -101,20 +80,14 @@ public class QueueTest {
 		System.out.println("Should print 1: " + Q7.getFront());
 		System.out.println();
 
-		
-		
-		
-		
-		
-		
 		System.out.println("************************************* Test(8) equals()\n");
-		Queue<Integer> Q8 = new Queue<>();		
+		Queue<Integer> Q8 = new Queue<>();
 		Queue<Integer> Q8copy = new Queue<>();
-		
+
 		Q8.enqueue(1);
 		Q8.enqueue(2);
 		Q8.enqueue(3);
-		
+
 		Q8copy.enqueue(0);
 		Q8copy.enqueue(1);
 		Q8copy.enqueue(2);
@@ -124,27 +97,22 @@ public class QueueTest {
 		System.out.println("Should print true: " + Q8.equals(Q8copy));
 		System.out.println();
 
-		
-		
 		System.out.println("************************************* Test(9) Copy Constructor()\n");
-		Queue<Integer> Q9 = new Queue<>();		
-		Queue<Integer> Q9copy = new Queue<>(Q9);		
-		
+		Queue<Integer> Q9 = new Queue<>();
+		Queue<Integer> Q9copy = new Queue<>(Q9);
+
 		System.out.println("Should print true: " + Q9.equals(Q9copy));
 		Q9.enqueue(1);
 		Q9.enqueue(2);
 		Q9.enqueue(3);
-		
+
 		System.out.println("Should print false: " + Q9.equals(Q9copy));
 
-		
-		Queue<Integer> Q9copy2 = new Queue<>(Q9);		
+		Queue<Integer> Q9copy2 = new Queue<>(Q9);
 
 		System.out.println("Should print true: " + Q9.equals(Q9copy2));
 		System.out.println("Should print 1 2 3: " + Q9copy2);
 
-
-		
 		System.out.println("******************************************************");
 	}
 }
